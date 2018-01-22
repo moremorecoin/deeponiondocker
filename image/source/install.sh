422a5a53ecb519deea0e0e0fce23c39df1526273
More Coin <moremorecoin@gmail.com>
2018-01-21 21:37:02 -0800

    v2.0 1. Backup wallet file to your email (Please, always encrypt your wallet!!) 2. Fixed some minor bugs

--- image/source/install.sh
+++ image/source/install.sh
@@ -1,25 +1,16 @@
-    apt-get update
-    apt-get install -y build-essential libtool autotools-dev autoconf pkg-config libssl-dev apt-utils
-    apt-get install -y libboost-all-dev
-    apt-get install -y libqrencode-dev
-    apt-get install -y libminiupnpc-dev
-    apt-get install -y libevent-dev
-    apt-get install -y git
+apt-get update
+apt-get install -y build-essential libtool autotools-dev autoconf pkg-config libssl-dev apt-utils libboost-all-dev libqrencode-dev libminiupnpc-dev libevent-dev git software-properties-common libjson-pp-perl
+add-apt-repository ppa:bitcoin/bitcoin
+apt-get update 
+apt-get install -y --allow-unauthenticated libdb4.8-dev libdb4.8++-dev
 
-    apt-get install -y software-properties-common
-    add-apt-repository ppa:bitcoin/bitcoin
-    #echo "deb-src http://ppa.launchpad.net/bitcoin/bitcoin/ubuntu zesty main" > /etc/apt/sources.list.d/bitcoin-ubuntu-bitcoin-bionic.list
-    #echo "deb http://ppa.launchpad.net/bitcoin/bitcoin/ubuntu zesty main" >> /etc/apt/sources.list.d/bitcoin-ubuntu-bitcoin-bionic.list
-    apt-get update 
-    apt-get install -y --allow-unauthenticated libdb4.8-dev libdb4.8++-dev
-
-    git clone https://github.com/deeponion/deeponion.git
-    cd deeponion
-    cd src
-    cd leveldb/
-    chmod 755 build_detect_platform
-    cd ..
-    make -f makefile.unix
-    cp DeepOniond /bin/
-    cd ../..
-    rm -rf deeponion
+git clone https://github.com/deeponion/deeponion.git
+cd deeponion
+cd src
+cd leveldb/
+chmod 755 build_detect_platform
+cd ..
+make -f makefile.unix
+cp DeepOniond /bin/
+cd ../..
+rm -rf deeponion
